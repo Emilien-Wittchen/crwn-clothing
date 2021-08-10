f75a5b22f1736f1b72295fde68ccf84f1d09aeec
Emilien-Wittchen <abizw97@gmail.com>
2021-08-10 18:57:05 +0200

    creating sign-in component, form-input, custom-button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 // const HatsPage = () => (
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/sign-in' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
