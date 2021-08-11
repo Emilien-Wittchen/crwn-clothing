23fee7e0bcfce95146fb231de6d53d21b79e1bcc
Emilien-Wittchen <abizw97@gmail.com>
2021-08-11 20:13:11 +0200

    finishing sign-in handleSubmit method, adding alert when sign-in fails

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,10 +14,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({email: '', password: ''});
+    const {email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''});
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
   };
 
   handleChange = (event) => {
@@ -38,7 +46,7 @@ class SignIn extends React.Component {
             type='email'
             value={this.state.email}
             handleChange={this.handleChange}
-            label='email'
+            label='Email'
             required
           />
 
@@ -47,7 +55,7 @@ class SignIn extends React.Component {
             type='password'
             value={this.state.password}
             handleChange={this.handleChange}
-            label='password'
+            label='Password'
             required
           />
           <div className='buttons'>
